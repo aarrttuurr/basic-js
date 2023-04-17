640e47c87bb133c451384d9f72cc5d2e73300517
Artur Kandaurau <kondaurov.archi@gmail.com>
2023-04-18 02:51:25 +0300

    one more

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if(typeof sampleActivity !== 'string' || typeof +sampleActivity !== 'number' || Number.isInteger(sampleActivity) || +sampleActivity < 0) {
+    return false;
+  }
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  let ln = Math.log(MODERN_ACTIVITY / sampleActivity);
+
+  if ((Math.ceil(ln / k)) === Infinity || (Math.ceil(ln / k)) < 0) {
+    return false;
+  }
+  return Math.ceil(ln / k);
 }
 
 module.exports = {
